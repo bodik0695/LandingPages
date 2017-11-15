322d4683ac9a8ca8032e121a8b1f3616a5871d83
bodik0695 <bodik9595@gmail.com>
2017-11-15 23:40:17 +0200

    adaptive and cross-browser

---
+++ MySecondLandingPage/jquery/jqueryForSecondProject.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+	var mobMenu = $(".navBar_mobileMenu");
+	var listItem = $(".pageMenu_listItem");
+	var hidden = true;
+	var hideMobMenu = false;
+
+	function hideMenu(){
+		listItem.css({'display': 'none'});
+		hidden = true;
+	}; //end hideMenu
+
+	function showMenu(){
+		listItem.css({
+			'display': 'inline-block'
+		});
+		hidden = false;
+	}; // end showMenu
+
+	mobMenu.click(function(){
+		if(hidden){
+			showMenu();
+		}
+		else{
+			hideMenu();
+		}
+	}); //end mobMenu.click
+
+	setInterval(function(){
+		var displayMobMenu = mobMenu.css('display');
+		if(displayMobMenu !== "block"){
+			listItem.css({
+				'display': 'inline-block'
+			});
+			hideMobMenu = false;
+			hidden = false;
+		}
+		else if(displayMobMenu === "block" && hideMobMenu == false){
+			listItem.css({'display': 'none'});
+			hideMobMenu = true;
+			hidden = true;
+		}
+	},1); //end setInterval
+}); // end script
